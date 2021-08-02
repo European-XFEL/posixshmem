947f74432e01e946c7f68c7994e99248603d7dc0
Philipp Schmidt <philipp.schmidt@xfel.eu>
2021-08-02 13:24:19 +0200

    Add first working prototype

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import Distribution, setup, find_packages
+from setuptools.extension import Extension
+
+# Fetch numpy and Cython as build dependencies.
+Distribution().fetch_build_eggs(['numpy', 'Cython'])
+
+from Cython.Build import cythonize  # noqa: E402
+
+
+setup(
+    name='posixshmem',
+    version='0.1.0',
+    description='Small python library to access POSIX shared memory.',
+    author='Philipp Schmidt',
+    author_email='philipp.schmidt@xfel.eu',
+
+    python_requires='>=3.6',
+    ext_modules=cythonize([
+        Extension('posixshmem', ['src/posixshmem.pyx'],
+                  extra_compile_args=['-g0', '-O2', '-fpic']),
+    ], language_level=3, build_dir='build'),
+)
---
+++ src/posixshmem.pyx
@@ -0,0 +1,113 @@
+
+from posix.fcntl cimport O_RDONLY, O_RDWR, O_CREAT
+from posix.mman cimport PROT_READ, PROT_WRITE, MAP_SHARED, MAP_PRIVATE, \
+    shm_open, shm_unlink, mode_t
+from posix.stat cimport struct_stat, fstat
+from posix.unistd cimport ftruncate
+from libc.errno cimport errno
+
+from mmap import mmap
+
+
+cdef class SharedMemory:
+    cdef int _fd
+    cdef object _buf
+    cdef object _view
+
+    cdef object _name
+    cdef size_t _size
+    cdef bint _rw
+    cdef int _mmap_flags
+
+    def __cinit__(self, object name, size_t size=0, bint rw=True,
+                  mode_t mode=0o777, mode_t shm_flags=0, mode_t mmap_flags=0):
+        self._name = name.encode('ascii')
+        self._rw = rw
+        self._buf = None
+        self._view = None
+
+        if size == 0 and rw:
+            raise ValueError('size must be specified if not read only')
+
+        if shm_flags == 0:
+            shm_flags = (O_CREAT | O_RDWR) if rw else O_RDONLY
+
+        self._fd = shm_open(self._name, shm_flags, mode)
+        if self._fd == -1:
+            raise OSError(errno)
+        
+        if rw:
+            if ftruncate(self._fd, size) == -1:
+                raise OSError(errno)
+
+            self._mmap_flags = mmap_flags or (PROT_WRITE | PROT_READ)
+        else:
+            self._mmap_flags = mmap_flags or PROT_READ
+
+        self._size = self._get_size()
+
+    cdef size_t _get_size(self):
+        cdef struct_stat stat
+        if fstat(self._fd, &stat) == -1:
+            raise OSError(errno)
+
+        return stat.st_size
+
+    cdef object _get_buf(self):
+        if self._buf is None:
+            self._buf = mmap(self._fd, self._size, MAP_SHARED,
+                             self._mmap_flags)
+
+        return self._buf
+
+    cdef object _get_view(self):
+        if self._view is None:
+            self._view = memoryview(self._get_buf())
+
+        return self._view
+
+    def __dealloc__(self):
+        if self._view is not None:
+            self._view = None
+
+        if self._buf is not None:
+            self._buf.close()
+            self._buf = None
+
+        if self._name is not None and self._rw:
+            if shm_unlink(self._name) == -1:
+                raise OSError(errno)
+        
+        self._name = None
+
+    @property
+    def fd(self):
+        return self._fd
+
+    @property
+    def buf(self):
+        return self._get_buf()
+
+    @property
+    def view(self):
+        return self._get_view()
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def name(self):
+        return self._name.decode('ascii')
+
+    def ndarray(self, dtype, shape=None, offset=0):
+        import numpy
+
+        cdef int count = self._size / dtype.itemsize
+        a = numpy.frombuffer(self._get_view(), dtype=dtype, count=count,
+                             offset=offset)
+
+        if shape is not None:
+            a = a.reshape(*shape)
+
+        return a
